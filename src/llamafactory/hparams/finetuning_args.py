07fd0c3a12d436ca4d5ca00171cc287681e4dd6c
Arda Uzunoğlu <ardauzunogluarda@gmail.com>
2025-03-15 21:41:51 +0300

    Update finetuning_args.py

--- src/llamafactory/hparams/finetuning_args.py
+++ src/llamafactory/hparams/finetuning_args.py
@@ -401,6 +401,10 @@ class FinetuningArguments(
         default=False,
         metadata={"help": "Whether or not to train model in purely bf16 precision (without AMP)."},
     )
+    freeze_neurons_config_file: str = field(
+        default="",
+        metadata={"help": "Which specific neurons to freeze."},
+    )
     stage: Literal["pt", "sft", "rm", "ppo", "dpo", "kto"] = field(
         default="sft",
         metadata={"help": "Which stage will be performed in training."},
